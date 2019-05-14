9bb0d570c51a29c0177af4f8bca5695347be278b
sanlog9595 <49307730+sanlog9595@users.noreply.github.com>
2019-05-14 19:47:40 +0530

    swap4.java

---
+++ swap4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Swa
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c;
+		c=a*b;
+		b=c/b;
+		a=c/b;
+		System.out.println(A+" "+B);
+	}
+}
